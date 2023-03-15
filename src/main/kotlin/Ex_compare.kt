63dfd5620789507f0bcff6f98c33b91295b8fb1f
eMobilis <sandrasasimwa@gmail.com>
2023-03-15 10:01:26 +0300

    exe comparison

---
+++ src/main/kotlin/Ex_compare.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+
+    print("Enter Number")
+    var number:Int = readln()!!.toInt()
+    if (number==1) {
+        print("Data Deals")
+    }
+    else if (number==2) {
+        print("Daily Bundles")
+    }
+    else if (number==3) {
+        print("Weekly Bundles")
+    }
+    else if (number==4) {
+        print("Go Monthly")
+    }
+    else if (number==5) {
+        print("No Expiry")
+    }
+    else{
+        print("Menu")
+    }
+
+
+
+
+
+
+
+
+}
