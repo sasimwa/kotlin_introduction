6ac4166df6aea2a0a57b0b2de94a9012bf5cad42
eMobilis <sandrasasimwa@gmail.com>
2023-03-21 09:01:32 +0300

    when statements

---
+++ src/main/kotlin/Ex_whenStatements.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+    print("Enter Option")
+    var point : Int = readln()!!.toInt()
+
+    when(point) {
+
+
+        0 -> println("Unlock Your Unlimited Storo Bonus")
+        1 -> println("Data Deals")
+        2 -> println("Tunukiwa Minutes")
+        3 -> println("Lipa Mdogo Mdogo")
+        4 -> println("Okoa Bundles")
+
+
+        else -> println("Thank You For Using Safaricom")
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
