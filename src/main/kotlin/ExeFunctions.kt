1047d018dded7ba0deef940e74fa2aaf665d5bea
eMobilis <sandrasasimwa@gmail.com>
2023-03-21 09:47:10 +0300

    functions kotln

---
+++ src/main/kotlin/ExeFunctions.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+
+    add()
+    multiply()
+    divide()
+
+
+
+}
+fun add(){
+    var num1 = 400
+    var num2 = 400
+    var result = num1 + num2
+    println(result)
+
+}
+fun multiply(){
+    var num1 = 500
+    var num2 = 4
+    var result = num1 * num2
+    println(result)
+
+}
+fun divide(){
+    var num1 = 100
+    var num2 = 4
+    var result = num1 / num2
+    println(result)
+
+}
